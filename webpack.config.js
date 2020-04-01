5c635afbca883f77d964ceb3ddbce7626d7aa0e2
stolbovd <stolbovd@gmail.com>
2020-04-01 17:59:08 +0500

    Typescript works

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,38 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
+	entry: './src/index.ts',
+	devtool: 'inline-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				use: [
+					// style-loader
+					{loader: 'style-loader'},
+					// css-loader
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true
+						}
+					},
+					// sass-loader
+					{loader: 'sass-loader'}
+				]
+			}
+		]
 	},
+	resolve: {
+		extensions: [ '.tsx', '.ts', '.js' ],
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	}
 };
