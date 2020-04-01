2e204e147301837c9255511c60daedfa9f27b601
stolbovd <stolbovd@gmail.com>
2020-04-01 20:35:42 +0500

    SASS works, but inside main.js

--- command.sh
+++ command.sh
@@ -4,3 +4,9 @@ npm init -y
 npm install webpack webpack-cli --save-dev
 npm install --save lodash
 npx webpack
+npx webpack --config webpack.config.js
+npm install --save-dev css-loader ts-loader typescript scss-loader
+npm install --save-dev @types/lodash
+npm install style-loader sass-loader node-sass --save-dev
+npm install fibers --save-dev
+npx webpack --config webpack.config.js
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,70 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/index.ts',
-	devtool: 'inline-source-map',
+	// devtool: 'source-map', // any "source-map"-like devtool is possible
+	mode: 'production',
+	// entry: './src/app.ts',
 	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.css$/,
-				use: [
-					// style-loader
-					{loader: 'style-loader'},
-					// css-loader
-					{
-						loader: 'css-loader',
-						options: {
-							modules: true
-						}
-					},
-					// sass-loader
-					{loader: 'sass-loader'}
-				]
-			}
-		]
+		rules: [{
+			test: /\.tsx?$/,
+			use: 'ts-loader',
+			exclude: /node_modules/,
+		}, {
+			test: /\.css$/,
+			use: [
+				'style-loader',
+				'css-loader',
+				'sass-loader'
+			]
+		}, {
+			// }, {
+			// 	test: /\.css$/,
+			// 	use: [
+			// 		// style-loader
+			// 		{loader: 'style-loader'},
+			// 		// css-loader
+			// 		{
+			// 			loader: 'css-loader',
+			// 			options: {
+			// 				modules: true
+			// 			}
+			// 		},
+			// 		// sass-loader
+			// 		{loader: 'sass-loader'}
+			// 	]
+			// }, {
+			// 	test: /\.s[ac]ss$/i,
+			// 	use: [
+			// 		// Creates `style` nodes from JS strings
+			// 		'style-loader',
+			// 		// Translates CSS into CommonJS
+			// 		{
+			// 			loader: 'css-loader',
+			// 			options: {
+			// 				sourceMap: false,
+			// 			},
+			// 		},
+			// 		// Compiles Sass to CSS
+			// 		{
+			// 			loader: 'sass-loader',
+			// 			options: {
+			// 				// Prefer `dart-sass`
+			// 				// implementation: require('sass'),
+			// 				// sassOptions: {
+			// 				// 	// fiber: require('fibers'),
+			// 				sourceMap: false,
+			// 				// 	webpackImporter: false,
+			// 				// },
+			// 			},
+			// 		},
+			// 	],
+		},]
 	},
 	resolve: {
-		extensions: [ '.tsx', '.ts', '.js' ],
+		extensions: ['.ts', '.js', '.sass', 'css'],
 	},
 	output: {
-		filename: 'bundle.js',
+		// filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
 	}
 };
